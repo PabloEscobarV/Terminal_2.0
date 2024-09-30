b7f890e7636f4eb09c87591a81bbdcc8ee049abf
Pablo Escobar <sataniv.rider@gmail.com>
2024-09-30 21:39:11 +0200

    Terminal: varhanler: added varhandler

---
+++ hdrs/terminalmacros.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   terminalmacros.h                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Pablo Escobar <sataniv.rider@gmail.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/30 20:33:12 by Pablo Escob       #+#    #+#             */
+/*   Updated: 2024/09/30 20:33:43 by Pablo Escob      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef TERMINAL_H
+# define TERMINAL_H
+
+#define SPLTCH		' '
+#define SLASH		'/'
+#define BKSLASH		'\\'
+#define VARCH		'$'
+#define SPCCH		' '
+#define PROCID		"$$"
+#define ESCSMBS		"\\$\"\'"
+#define MALLOCERROR	"MEMMORY ALLOCATE ERROR!!!"
+#define TOKENERROR	"syntax error near unexpected token"
+#define HRDOC		"<<"
+#define QTS			"\" \'"
+#define SPLN		"|| $$ | $"
+#define RDR			">> > <"
+#define SPLTS		"<< >> || < > | \" \'"
+
+#endif
